a3c7e0e7da4b206a4347fd23e9902e4d7484b82f
gare113 <38461269+gare113@users.noreply.github.com>
2018-11-28 11:35:16 +0000

    challenge class(not complete)

---
+++ OOP-master/preTOC/src/pretoc/Challenge.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pretoc;
+
+/**
+ *
+ * @author Desiree
+ */
+public class Challenge {
+    
+    private int uniqueNumber;
+    private String Type;
+    private String Enemy;
+    private int skillRequired;
+    private int Reward;
+    
+    public Challenge (int uniqueNumber, String Type, String Enemy, int skillRequired, int Reward ) {
+        this.uniqueNumber = uniqueNumber;
+        this.Type = Type;
+        this.Enemy = Enemy;
+        this.skillRequired = skillRequired;
+        this.Reward = Reward;
+    }
+    
+    
+    public int getNumber (){
+        
+    }
+    
+    
+    
+}
---
+++ OOP-master/preTOC/src/pretoc/ChallengeList.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pretoc;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author Desiree
+ */
+public class ChallengeList {
+    
+    public static void main(String [] args) {
+        list<String> Challenge = new Array:ost<>();
+        Challenge.add()
+    }
+    
+    
+}
